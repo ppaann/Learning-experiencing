afe71281123036b4faa4a6522459e090a4dc45c1
ppaann <reg.ppan@gmail.com>
2015-11-06 21:24:57 +0200

    start Adding angular

---
+++ js/myapp.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var myApp = angular.module('myApp', ['ngRoute'])
+	.config(function($routeProvider)
+	{
+		$routeProvider.when('/',
+		{
+			templateUrl: 'list/list.html'
+		});
+		$routeProvider.when('/css-shade-progressbar',
+		{
+			templateUrl: 'css-shade-progressbar/css-shade-progressbar.html'
+		})
+	});
